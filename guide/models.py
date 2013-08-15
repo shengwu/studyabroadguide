c2796402615c88f37fb44f86db3aff620888270c
Sheng Wu <pkernoobie@gmail.com>
2013-08-15 01:18:11 -0700

    Add model labels for admin

--- guide/models.py
+++ guide/models.py
@@ -6,6 +6,9 @@ class Place(models.Model):
     overview = models.TextField(blank=True, null=True)
     slug = models.SlugField()
 
+    def __unicode__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
@@ -14,13 +17,28 @@ class Place(models.Model):
 class Photo(models.Model):
     place = models.ForeignKey(Place)
 
+    def __unicode__(self):
+        return u'Photo %d at %s' % (self.id, self.place)
+
 class Program(models.Model):
     place = models.ForeignKey(Place)
     location = models.CharField(max_length=40)
     for_credit = models.BooleanField()
+
+    def __unicode__(self):
+        return u'Program: %s at %s' % (self.location, self.place)
 
 class Tip(models.Model):
     text = models.TextField(blank=True, null=True)
     category = models.CharField(max_length=40, blank=True, null=True)
     place = models.ForeignKey(Place, null=True)
     program = models.ForeignKey(Program, null=True)
+
+    def __unicode__(self):
+        if self.place:
+            location = self.place.__unicode__()
+        elif self.program:
+            location = self.program.__unicode__()
+        else:
+            location = ''
+        return u'Tip: "%s" for %s' % (self.text, location)
